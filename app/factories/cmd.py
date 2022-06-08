b09b86499cb22fff38fbc32ece8f7b0016536ba7
nurivansyah <m.nurivansyah@gmail.com>
2022-06-08 16:58:12 +0700

    add cmd for check ospf conn

--- app/factories/cmd.py
+++ app/factories/cmd.py
@@ -4,6 +4,8 @@ from app.cmds.test_cmd import TestCmd
 from app.cmds.bp_ospf_auth_iosxe_cmd import BPOSPFAuthIOSXECmd
 from app.cmds.bp_ospf_auth_iosxr_cmd import BPOSPFAuthIOSXRCmd
 from app.cmds.show_pre_post_check_ios_cmd import ShowPrePostCheckIOSCmd
+from app.cmds.check_ospf_connectivity_iosxe_cmd import CheckOSPFConnectivityIOSXECmd
+from app.cmds.check_ospf_connectivity_iosxr_cmd import CheckOSPFConnectivityIOSXRCmd
 
 
 class CmdOption(Enum):
@@ -11,6 +13,8 @@ class CmdOption(Enum):
     BP_OSPF_AUTH_IOSXE = "bp_ospf_auth_iosxe"
     BP_OSPF_AUTH_IOSXR = "bp_ospf_auth_iosxr"
     SHOW_PRE_POST_CHECK_IOS = "show_pre_post_check_ios"
+    CHECK_OSPF_CONNECTIVITY_IOSXE = "check_ospf_connectivity_iosxe"
+    CHECK_OSPF_CONNECTIVITY_IOSXR = "check_ospf_connectivity_iosxr"
 
 
 CMD_FACTORIES = {
@@ -18,6 +22,8 @@ CMD_FACTORIES = {
     CmdOption.BP_OSPF_AUTH_IOSXE: BPOSPFAuthIOSXECmd,
     CmdOption.BP_OSPF_AUTH_IOSXR: BPOSPFAuthIOSXRCmd,
     CmdOption.SHOW_PRE_POST_CHECK_IOS: ShowPrePostCheckIOSCmd,
+    CmdOption.CHECK_OSPF_CONNECTIVITY_IOSXE: CheckOSPFConnectivityIOSXECmd,
+    CmdOption.CHECK_OSPF_CONNECTIVITY_IOSXR: CheckOSPFConnectivityIOSXRCmd,
 }
 
 
