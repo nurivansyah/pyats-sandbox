102d71d96e9347fba352bf3fd302e7d8e2fc770e
nurivansyah <m.nurivansyah@gmail.com>
2022-06-08 16:40:52 +0700

    add cmd for for bp ospf auth

--- app/factories/cmd.py
+++ app/factories/cmd.py
@@ -1,14 +1,20 @@
 from enum import Enum
 from app.cmds.base import ConnectedDeviceCmd
 from app.cmds.test_cmd import TestCmd
+from app.cmds.bp_ospf_auth_iosxe_cmd import BPOSPFAuthIOSXECmd
+from app.cmds.bp_ospf_auth_iosxr_cmd import BPOSPFAuthIOSXRCmd
 
 
 class CmdOption(Enum):
     TEST = "test_cmd"
+    BP_OSPF_AUTH_IOSXE = "bp_ospf_auth_iosxe"
+    BP_OSPF_AUTH_IOSXR = "bp_ospf_auth_iosxr"
 
 
 CMD_FACTORIES = {
     CmdOption.TEST: TestCmd,
+    CmdOption.BP_OSPF_AUTH_IOSXE: BPOSPFAuthIOSXECmd,
+    CmdOption.BP_OSPF_AUTH_IOSXR: BPOSPFAuthIOSXRCmd,
 }
 
 
