bf48fe6ca3307eab5748574fe257027d07d0ca2b
nurivansyah <m.nurivansyah@gmail.com>
2022-06-08 16:49:42 +0700

    add cmd for show prepost activity

--- app/factories/cmd.py
+++ app/factories/cmd.py
@@ -3,18 +3,21 @@ from app.cmds.base import ConnectedDeviceCmd
 from app.cmds.test_cmd import TestCmd
 from app.cmds.bp_ospf_auth_iosxe_cmd import BPOSPFAuthIOSXECmd
 from app.cmds.bp_ospf_auth_iosxr_cmd import BPOSPFAuthIOSXRCmd
+from app.cmds.show_pre_post_check_ios_cmd import ShowPrePostCheckIOSCmd
 
 
 class CmdOption(Enum):
     TEST = "test_cmd"
     BP_OSPF_AUTH_IOSXE = "bp_ospf_auth_iosxe"
     BP_OSPF_AUTH_IOSXR = "bp_ospf_auth_iosxr"
+    SHOW_PRE_POST_CHECK_IOS = "show_pre_post_check_ios"
 
 
 CMD_FACTORIES = {
     CmdOption.TEST: TestCmd,
     CmdOption.BP_OSPF_AUTH_IOSXE: BPOSPFAuthIOSXECmd,
     CmdOption.BP_OSPF_AUTH_IOSXR: BPOSPFAuthIOSXRCmd,
+    CmdOption.SHOW_PRE_POST_CHECK_IOS: ShowPrePostCheckIOSCmd,
 }
 
 
